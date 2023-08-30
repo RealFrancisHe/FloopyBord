9c7f709d73da2951d3a8cdeabc0edb1e93ecb057
unknown <fran.he.fury@gmail.com>
2023-08-30 16:00:37 -0400

    Functional Prototype A

---
+++ Assets/LogicScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+
+    [ContextMenu("Increase Score")] // adds to context menu
+    public void addScore(int scoreToAdd)
+    {
+        playerScore = playerScore + scoreToAdd;
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+}
